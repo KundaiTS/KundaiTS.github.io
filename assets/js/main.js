a22a512cda0617a0952ceb03747272960dfd3f1e
Kundai <143875591+KundaiTS@users.noreply.github.com>
2025-02-20 10:43:49 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
-/*
-	Story by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
 
 (function($) {
 
@@ -350,8 +346,6 @@
                 navbar.classList.remove("scrolled");
             }
         });
-document.getElementById("menu-toggle").addEventListener("click", function () {
+   document.getElementById("menu-toggle").addEventListener("click", function () {
     document.querySelector("#navbar ul").classList.toggle("active");
 });
-
-   
